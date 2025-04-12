8a5f0dee98ab99f3e7fa8d77dc0284288cf1d569
thoufeekx <thoufeekbaber1@gmail.com>
2025-04-12 00:08:41 -0400

    fix: add proper type specifications to variables
    - Added explicit type declarations for all variables
    - Added descriptive comments for each variable
    - Ensured proper formatting of variable blocks

--- terraform/environments/dev/development.tf
+++ terraform/environments/dev/development.tf
@@ -5,23 +5,35 @@ variable "dev_mode" {
 }
 
 variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
 }
 
 variable "location" {
+  description = "Azure region for resources"
+  type        = string
 }
 
 variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
 }
 
 variable "api_server_authorized_ip_ranges" {
-  default = []
+  description = "List of IP ranges allowed to access the API server"
+  type        = list(string)
+  default     = []
 }
 
 variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
 }
 
 variable "allowed_cidr" {
-  default = ""
+  description = "CIDR range for allowed network access"
+  type        = string
+  default     = ""
 }
 
 locals {
@@ -32,22 +44,22 @@ locals {
 
 module "aks" {
   source = "../modules/aks"
-  
-  cluster_name = var.cluster_name
-  location     = var.location
+
+  cluster_name        = var.cluster_name
+  location            = var.location
   resource_group_name = var.resource_group_name
-  
+
   # Development-specific overrides
   api_server_authorized_ip_ranges = local.dev_api_access ? ["0.0.0.0/0"] : var.api_server_authorized_ip_ranges
 }
 
 module "network" {
   source = "../modules/network"
-  
-  vnet_name = var.vnet_name
-  location  = var.location
+
+  vnet_name           = var.vnet_name
+  location            = var.location
   resource_group_name = var.resource_group_name
-  
+
   # Development-specific overrides
   allowed_cidr = local.dev_network_rules ? "0.0.0.0/0" : var.allowed_cidr
 }
