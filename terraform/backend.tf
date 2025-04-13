2417fe8a9430343708cda181ed9d57d865082cf1
thoufeekx <thoufeekbaber1@gmail.com>
2025-04-13 01:56:02 -0400

    chore: Switch to local backend to resolve Azure container issues

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-fp6-rg"
-    storage_account_name = "tfstatepbo79qfk"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
